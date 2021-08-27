782391d8886d9104cc652fb8d947bcf144d55b88
Baeerd <1084415787@qq.com>
2021-08-27 16:12:15 +0800

    用户信息界面

--- app.js
+++ app.js
@@ -13,6 +13,34 @@ App({
       }
     })
   },
+
+  // 登录
+  login() {
+    let me = this;
+    wx.login({
+      success (res) {
+        if (res.code) {
+          // 调用 auth.code2Session，使用 code 换取 openid、unionid、session_key 等信息
+          wx.request({
+            url: 'https://api.weixin.qq.com/sns/jscode2session',
+            data: {
+              appid: 'wxcec2524213396666',
+              secret:'6e8d9bd155886b2a37ba56515322af43',
+              js_code: res.code,
+              grant_type: 'authorization_code'
+            },
+            success (res) {
+              console.log(res)
+              me.setData({openId: res.data.openid});
+            }
+          })
+        } else {
+          console.log('登录失败！' + res.errMsg)
+        }
+      }
+    })
+  },
+
   globalData: {
     userInfo: null
   }
